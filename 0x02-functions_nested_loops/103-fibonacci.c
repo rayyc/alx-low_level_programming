9c5b63ee223a7036e3059e4c4ce0a4f2605d3a10
rayyc <rayw68449@gmail.com>
2023-03-26 13:07:02 -0700

    task 15

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,24 +1,23 @@
 #include <stdio.h>
-
 /**
- * main - Prints the sum of even fibobacci numbers
- * less than 4000000.
- * Return: Nothing!
+ * main - main block
+ * Description: computes and prints even number < 4,000,000
+ * 5 below 1024 (excluded), followed by a new line
+ * Return: 0
  */
-
 int main(void)
 {
-	int i = 0;
-	long j = 1, k = 2;
+	int a = 0, b = 1, next = 0;
+	int sum = 0;
 
-	while (k + j < 4000000)
+	while (next < 4000000)
 	{
-	k += j;
-	if (k % 2 == 0)
-	sum += k;
-	j = k - j;
-	++i;
+		next = a + b;
+		a = b;
+		b = next;
+		if (next % 2 == 0)
+			sum += next;
 	}
-	printf("%ld\n", sum);
+	printf("%i\n", sum);
 	return (0);
 }
